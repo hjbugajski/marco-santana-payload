4b1dd6a63a15c90ac8d74a9c81022d49a49020fc
Henry Bugajski <henry.bug.dev@gmail.com>
2022-11-16 13:02:00 -0500

    feat: update Users collection, add access utils, add payload-types

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,5 +1,7 @@
 import { CollectionConfig } from 'payload/types';
 
+import { isAdminFieldLevel, isAdminOrSelf, isAdminOrSelfFieldLevel } from '../utils/access';
+
 const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
@@ -8,9 +10,40 @@ const Users: CollectionConfig = {
   },
   access: {
     create: () => true,
-    read: () => true
+    read: isAdminOrSelf,
+    update: isAdminOrSelf,
+    delete: isAdminOrSelf
   },
-  fields: []
+  fields: [
+    {
+      type: 'row',
+      fields: [
+        {
+          name: 'firstName',
+          type: 'text',
+          required: false
+        },
+        {
+          name: 'lastName',
+          type: 'text',
+          required: false
+        }
+      ]
+    },
+    {
+      name: 'roles',
+      type: 'select',
+      hasMany: true,
+      defaultValue: ['public'],
+      required: true,
+      access: {
+        read: isAdminOrSelfFieldLevel,
+        create: isAdminFieldLevel,
+        update: isAdminFieldLevel
+      },
+      options: ['admin', 'editor', 'public']
+    }
+  ]
 };
 
 export default Users;
